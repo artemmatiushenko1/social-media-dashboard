26677981f0fd3e71cb6288f3b2a5dccb06fc32a1
artemmatiushenko1 <artom.matyushenko@gmail.com>
2021-09-28 21:30:22 +0300

    Created and styled header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ToogleButton from './ToogleButton';
+import './Header.css';
+
+export default function Header(props) {
+  return (
+    <div>
+      <header className="header">
+        <div className="header__textbox">
+          <h1 className="heading-primary">Social Media Dashboard</h1>
+          <p className="paragraph">
+            Total Followers:{' '}
+            <span className="header__followers-count">23, 004</span>
+          </p>
+        </div>
+        <div className="header__color-mode-box">
+          <p className="paragraph">Dark Mode</p>
+          <ToogleButton className="header__theme-toogle-btn"></ToogleButton>
+        </div>
+      </header>
+    </div>
+  );
+}
---
+++ src/components/ToogleButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ToogleButton.css';
+
+export default function ToogleButton() {
+  return (
+    <div>
+      <input type="checkbox" className="checkboxBtn" />
+    </div>
+  );
+}
